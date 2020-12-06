29a32cd080ce7de27318bd3b7814bfc32ea3c99e
AndreaPiriz <68929750+AndreaPiriz@users.noreply.github.com>
2020-12-06 02:04:52 -0300

    usando grunt

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(function () {
+            $("[data-toggle='tooltip']").tooltip();
+            $("[data-toggle='popover']").popover();
+            $('.carousel').carousel({
+                interval: 3000
+            });
+
+            $('#contacto').on('show.bs.modal', function (e) {
+                console.log('El modal de contacto se está mostrando');
+
+                $('#contactarBTN').removeClass('btn-outline-success');
+                $('#contactarBTN').addClass('btn-primary');
+                $('#contactarBTN').prop('disabled', true);
+
+            });
+            $('#contacto').on('shown.bs.modal', function (e) {
+                console.log('El modal de contacto se mostró');
+            });
+            $('#contacto').on('hide.bs.modal', function (e) {
+                    console.log('El modal de contacto se oculta');
+                }),
+                $('#contacto').on('hidden.bs.modal', function (e) {
+                    console.log('El modal de contacto se ocultó');
+                });
+        });
